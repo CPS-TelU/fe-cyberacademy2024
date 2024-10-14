49bcc9c2033fede5d96d334cf045c47fe9fd3bcf
bdil.com <afnabil121002@gmail.com>
2024-10-14 18:57:27 +0700

    updateloginresgister

--- components/contents/Login.tsx
+++ components/contents/Login.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const LoginPage = () => {
   return (
-    <div className="relative min-h-screen w-full bg-[-fffff] flex items-center justify-center p-4">
-      <button className="absolute top-12 left-4 sm:left-72 flex items-center justify-center w-10 h-10 bg-red-600 text-white rounded-full z-10">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-        </svg>
-      </button>
+      <div className="relative min-h-screen w-full bg-[#ffffff] flex items-center justify-center p-4">
+        <button className="absolute top-12 left-4 sm:left-72 flex items-center justify-center w-10 h-10 bg-red-600 text-white rounded-full z-10 transform hover:scale-105 hover:bg-red-700 transition-colors duration-300 ease-in-out">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 transition-transform duration-300 ease-in-out hover:scale-110">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>
       <div className="flex flex-col items-center w-full max-w-2xl">
       <img src="LogoAbout.png" alt="Logo" className="w-48 sm:w-64 mx-auto mb-4 mt-1" />
         <div className="relative w-full bg-white rounded-lg shadow-lg p-6 sm:p-8 md:p-16 mt-4 border border-gray-200">
